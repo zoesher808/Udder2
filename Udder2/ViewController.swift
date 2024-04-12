0b849693f8981372788774dcec72a10367d2c5cb
Zoe Sher <zoesher28@gmail.com>
2024-04-11 18:02:55 -0700

    change view did disappear

--- Udder2/ViewController.swift
+++ Udder2/ViewController.swift
@@ -17,5 +17,9 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         //
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        //
+    }
 }
 
